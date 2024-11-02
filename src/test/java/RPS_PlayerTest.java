1d92f5395530be80a6f12927600b978f55b40801
jekutinamo23 <jekutinamo23@students.desu.edu>
2024-11-01 22:48:48 -0400

    Run

--- src/test/java/RPS_PlayerTest.java
+++ src/test/java/RPS_PlayerTest.java
@@ -1,136 +1,177 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import java.util.Random;
 
-class RPS_PlayerTest {
+public class RPS_Player {
+    private static final int rock = 0;
+    private static final int paper = 1;
+    private static final int scissors = 2;
+    private int numberOfGamesWon;
+    private int numberOfGamesPlayed;
+    private int choice;
+    private String name;
 
-    RPS_Player player1;
-    RPS_Player player2;
-    RPS_Player player3;
-
-    public RPS_PlayerTest(){
-
+    public RPS_Player(int numberOfGamesWon, int numberOfGamesPlayed, int choice, String name) {
+        this.numberOfGamesWon = numberOfGamesWon;
+        this.numberOfGamesPlayed = numberOfGamesPlayed;
+        this.choice = choice;
+        this.name = name;
     }
 
-    @BeforeEach
-    void createPlayers()
-    {
-        player1 = new RPS_Player("P1");
-        player2 = new RPS_Player("P2");
-        player3 = new RPS_Player("P3");
+    public RPS_Player(String name){
+        // TODO: replace this line with your code.
+        this.name = name;
     }
 
-    @Test
-    @DisplayName("[1] test getNumberOfGamesPlayed")
-    void getNumberOfGamesPlayed() {
+    public String getName(){
+        // TODO: replace this line with your code.
+        return name;
+    }
+    public int getChoice(){
+        return choice;
+    }
+
+    /**
+     * Returns the number of games played since a clear() was issued.
+     * @return returns the number of games played.
+     */
+    public int getNumberOfGamesPlayed(){
+        // TODO: replace this line with your code.
+        return numberOfGamesPlayed;
+    }
+
+    /**
+     * Returns the number of games won since a clear() was issued.
+     * @return returns the number of games won.
+     */
+    public int getNumberOfGamesWon(){
+        // TODO: replace this line with your code.
+        return numberOfGamesWon;
+    }
+
+    /**
+     * Returns the win percentage as number between 0 and 1.
+     * @return win percentage as a double.
+     */
+    public double getWinPercentage(){
+        // TODO: replace this line with your code.
+        double ngw = numberOfGamesWon;
+        double ngp = numberOfGamesPlayed;
+        return (ngw/ngp);
+    }
+
+    /**
+     * Starts a new game.
+     */
+    public void clear(){
+        // TODO: replace this line with your code.
+        this.numberOfGamesWon = 0;
+        this.numberOfGamesPlayed = 0;
+    }
+    /**
+     * This player challenges anotherPlayer whereby both players make a
+     * random choice of rock, paper, scissors.  A reference to the winning
+     * player is returned or null if there is a draw.
+     * @param anotherPlayer an RPS_Player that this player is challenging.
+     * @return Reference to the RPS_Player that won or a null if there is a draw
+     */
+    public RPS_Player challenge(RPS_Player anotherPlayer){
+        // TODO: replace this line with your code.
         Random rand = new Random();
-        int expected = rand.nextInt(20);
-        for(int i=0; i < expected; i++){
-            player1.challenge(player2);
-            player2.challenge(player3);
-            player3.challenge(player1);
+        choice = rand.nextInt(3);
+        anotherPlayer.choice = rand.nextInt(3);
+        if(choice == rock && anotherPlayer.choice == paper ){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
         }
-        assertEquals(2*expected,player1.getNumberOfGamesPlayed());
-        assertEquals(2*expected,player2.getNumberOfGamesPlayed());
-        assertEquals(2*expected,player3.getNumberOfGamesPlayed());
-    }
-
-    @Test
-    @DisplayName("[1] test getName")
-    void getName() {
-        assertEquals("P1", player1.getName());
-        assertEquals("P2", player2.getName());
-        assertEquals("P3", player3.getName());
-    }
-
-    @Test
-    @DisplayName("[1] test getNumberOfGamesWon")
-    void getNumberOfGamesWon() {
-        int p1Expected = 0;
-        int p2Expected = 0;
-
-        for(int i=0; i < 50; i++){
-            RPS_Player winner = player1.challenge(player2);
-            if (winner == player1){
-                p1Expected += 1;
-            }else if (winner == player2){
-                p2Expected +=1;
-            }
+        else if(choice == paper && anotherPlayer.choice == rock){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
         }
-        assertEquals(p1Expected,player1.getNumberOfGamesWon());
-        assertEquals(p2Expected,player2.getNumberOfGamesWon());
+        else if(choice == paper && anotherPlayer.choice == scissors){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
+        }
+        else if(choice == scissors && anotherPlayer.choice == paper){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
+        }
+        else if(choice == scissors && anotherPlayer.choice == rock){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
+        }
+        else if(choice == rock && anotherPlayer.choice == scissors){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
+        }
+        else if(choice == anotherPlayer.choice){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+        }
+
+        return null;
+
     }
 
-    @Test
-    @DisplayName("[1] test getWinPercentage")
-    void getWinPercentage() {
-        int p1Expected = 0;
-        int p2Expected = 0;
-        for(int i=0; i < 50; i++){
-            RPS_Player winner = player1.challenge(player2);
-            if (winner == player1){
-                p1Expected += 1;
-            }else if (winner == player2){
-                p2Expected +=1;
-            }
+    /**
+     * This player challenges anotherPlayer whereby this player uses the previous
+     * choice made and anotherPlayer makes a random choice of rock, paper, scissors.
+     * A reference to the winning player is returned or null if there is a draw.
+     * @param anotherPlayer an RPS_Player that this player is challenging.
+     * @return Reference to the RPS_Player that won or a null if there is a draw
+     */
+    public RPS_Player keepAndChallenge(RPS_Player anotherPlayer){
+        // TODO: replace this line with your code.
+        Random rand = new Random();
+        anotherPlayer.choice = rand.nextInt(3);
+        if(this.choice == rock && anotherPlayer.choice == paper ){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
         }
-        assertEquals(p1Expected/50.0,player1.getWinPercentage());
-        assertEquals(p2Expected/50.0,player2.getWinPercentage());
+        else if(this.choice == paper && anotherPlayer.choice == rock){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
+        }
+        else if(this.choice == paper && anotherPlayer.choice == scissors){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
+        }
+        else if(this.choice == scissors && anotherPlayer.choice == paper){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
+        }
+        else if(this.choice == scissors && anotherPlayer.choice == rock){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesWon++;
+            return anotherPlayer;
+        }
+        else if(this.choice == rock && anotherPlayer.choice == scissors){
+            this.numberOfGamesPlayed++;
+            anotherPlayer.numberOfGamesPlayed++;
+            this.numberOfGamesWon++;
+            return this;
+        }
+        return null;
+
     }
 
-    @Test
-    @DisplayName("[3] test clear")
-    void clear() {
-        for(int i=0; i < 50; i++){
-            player1.challenge(player2);
-            player2.challenge(player3);
-            player3.challenge(player1);
-        }
-        player1.clear();
-        player2.clear();
-        player3.clear();
-        assertEquals(0,player1.getNumberOfGamesPlayed());
-        assertEquals(0,player2.getNumberOfGamesPlayed());
-        assertEquals(0,player3.getNumberOfGamesPlayed());
-        assertEquals(0,player1.getNumberOfGamesWon());
-        assertEquals(0,player2.getNumberOfGamesWon());
-        assertEquals(0,player3.getNumberOfGamesWon());
-    }
-
-    @Test
-    @DisplayName("[6] test challenge")
-    void challenge() {
-        int p1Expected = 0;
-        int p2Expected = 0;
-        for(int i=0; i < 50; i++){
-            RPS_Player winner = player1.challenge(player2);
-            if (winner == player1){
-                p1Expected += 1;
-            }else if (winner == player2){
-                p2Expected +=1;
-            }
-        }
-        assertEquals(p1Expected,player1.getNumberOfGamesWon());
-        assertEquals(p2Expected,player2.getNumberOfGamesWon());
-    }
-
-    @Test
-    @DisplayName("[6] test keepAndChallenge")
-    void keepAndChallenge() {
-        int p1Expected = 0;
-        int p2Expected = 0;
-        for(int i=0; i < 50; i++){
-            RPS_Player winner = player1.keepAndChallenge(player2);
-            if (winner == player1){
-                p1Expected += 1;
-            }else if (winner == player2){
-                p2Expected +=1;
-            }
-        }
-        assertEquals(p1Expected,player1.getNumberOfGamesWon());
-        assertEquals(p2Expected,player2.getNumberOfGamesWon());
-    }
 }
